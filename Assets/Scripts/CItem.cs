2b57e259ea30c94b410fd7ba90bbea0efc04a5ba
Brunhild <nechwi1121@gmail.com>
2018-10-23 18:06:41 +0900

    Add Simple Inventory Project

---
+++ Assets/Scripts/CItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CItem
+{
+    public string title = string.Empty;
+    public string description = string.Empty;
+    public int id = -1;
+    public Sprite icon;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+
+    public CItem(int id, string title, string description, Dictionary<string, int> stats)
+    {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        this.stats = stats;
+    }
+
+    public CItem(CItem item)
+    {
+        id = item.id;
+        title = item.title;
+        description = item.description;
+        icon = Resources.Load<Sprite>("Sprites/Items/" + title);
+        stats = item.stats;
+    }
+}
---
+++ Assets/Scripts/CItemDatabase.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CItemDatabase : MonoBehaviour
+{
+    public List<CItem> items = new List<CItem>();
+
+    private void Awake()
+    {
+        BuildDatabase();
+        GetItem(2);
+    }
+
+    public CItem GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public CItem GetItem(string itemName)
+    {
+        return items.Find(item => item.title.Equals(itemName));
+    }
+
+    private void BuildDatabase()
+    {
+        items = new List<CItem>()
+        {
+            new CItem(0, "Diamond Sword", "A sword made of diamonds.",
+                new Dictionary<string, int>
+                {
+                    { "Power", 15},
+                    {"Defence", 10}
+                }),
+            new CItem(1, "Diamond Ore", "A pretty diamond",
+                new Dictionary<string, int>
+                {
+                    { "Value", 300}
+                }),
+            new CItem(2, "Silver Pick", "A pick that could kill a vampire",
+                new Dictionary<string, int>
+                {
+                    { "Power", 5},
+                    { "Mining", 20},
+                }),
+        };
+    }
+}
